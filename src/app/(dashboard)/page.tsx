a15f74c0c6fdadca42e7d62ca54a93fcefbb7994
guicodee <guilhermedfgo12@gmail.com>
2024-10-16 17:46:31 -0300

    feat: add overview component

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -3,6 +3,7 @@
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 import CreateNewTransaction from './components/create-new-transaction';
+import Overview from './components/overview';
 
 export default function Page() {
 	const { data, status } = useSession();
@@ -10,18 +11,23 @@ export default function Page() {
 	if (!data || !data.user) redirect('/sign-in');
 
 	return (
-		<div className="w-full bg-card h-24 px-5 py-4 mt-4 mr-4 border border-zinc-900/80 flex items-center justify-between">
-			<div>
-				<h1 className="text-2xl text-center">
-					Olá,{' '}
-					<span className="font-black">
-						{data.user.name?.split(' ')?.[0]}! 👋
-					</span>
-				</h1>
+		<div className="flex flex-col w-full mr-4 mt-2">
+			<div className="w-full bg-card h-24 px-5 py-4 mt-4 border border-zinc-900/80 flex items-center justify-between">
+				<div>
+					<h1 className="text-2xl text-center">
+						Olá,{' '}
+						<span className="font-black">
+							{data.user.name?.split(' ')?.[0]}! 👋
+						</span>
+					</h1>
+				</div>
+				<div className="flex items-center gap-4">
+					<CreateNewTransaction type="income" />
+					<CreateNewTransaction type="expense" />
+				</div>
 			</div>
-			<div className="flex items-center gap-4">
-				<CreateNewTransaction type="income" />
-				<CreateNewTransaction type="expense" />
+			<div className="mt-8">
+				<Overview />
 			</div>
 		</div>
 	);

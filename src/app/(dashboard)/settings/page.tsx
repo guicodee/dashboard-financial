f91abfc695d68990a5e0d11f75aca87127643cab
guicodee <guilhermedfgo12@gmail.com>
2024-10-17 18:35:57 -0300

    feat: add button of sign out and button delete account in settings page

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,7 +1,10 @@
 import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
+import ButtonDeleteAccount from './components/button-delete-account';
+import ButtonOut from './components/button-out';
 
 export default async function SettingsPage() {
 	const user = await getServerSession(authOptions);
@@ -14,12 +17,42 @@ export default async function SettingsPage() {
 				<div className="flex flex-col">
 					<h2 className="text-lg font-bold">Perfil</h2>
 					<p className="text-muted-foreground text-sm">
-						Isso será exibido no seu perfil
+						Isso é exibido no seu perfil
 					</p>
 				</div>
 				<div className="flex flex-col gap-3 w-96">
 					<Input value={user.user.email as string} disabled={true} />
 					<Input value={user.user.name as string} />
+				</div>
+			</div>
+			<div className="mt-12">
+				<Separator />
+				<div className="flex items-center mt-8 w-full gap-4">
+					<div className="flex flex-col">
+						<h2 className="text-lg font-bold">Sair</h2>
+						<p className="text-muted-foreground text-sm max-w-[300px]">
+							Para sair da sua conta, basta clicar no botão ao lado "Sair".
+						</p>
+					</div>
+					<div className="w-[300px]">
+						<ButtonOut />
+					</div>
+				</div>
+			</div>
+			<div className="mt-12">
+				<Separator />
+				<div className="flex items-center mt-8 w-full gap-4">
+					<div className="flex flex-col">
+						<h2 className="text-lg font-bold">Excluir conta</h2>
+						<p className="text-muted-foreground text-sm max-w-[300px]">
+							Caso queira excluir permanentemente a sua conta, basta clicar no
+							botão ao lado "Excluir".
+							<br />
+						</p>
+					</div>
+					<div className="w-[300px]">
+						<ButtonDeleteAccount />
+					</div>
 				</div>
 			</div>
 		</div>
